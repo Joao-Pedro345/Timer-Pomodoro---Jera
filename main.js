d09936059f75d4a70ba38eb006da8e30b76cbcb9
João Pedro Tavares <joao.pedro.ifms@gmail.com>
2021-07-04 17:40:27 -0400

    Timer pomodoro finalizado

--- main.js
+++ main.js
@@ -8,16 +8,50 @@ var ws = document.getElementById('w_seconds');
 var bm = document.getElementById('b_minutes');
 var bs = document.getElementById('b_seconds');
 
+var optionCounter = false;
+
+var option = false;
+
 var addButton = document.getElementById('add');
 var removeButton = document.getElementById('remove');
+var lastValue = 25;
+   //Após o carregamento da página
+   document.addEventListener('DOMContentLoaded', function () {
+
+  //Se não tiver suporte a Notification manda um alert para o usuário
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+  
+  //Se não tem permissão, solicita a autorização do usuário
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
+
+   //Após o carregamento da página
+   document.addEventListener('DOMContentLoaded', function () {
+ 
+  //Se não tiver suporte a Notification manda um alert para o usuário
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+  
+  //Se não tem permissão, solicita a autorização do usuário
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
 
 addButton.addEventListener('click',function(){
     wm.innerHTML++;
+    lastValue = wm.innerHTML;
 });
 
 removeButton.addEventListener('click',function(){
     if(wm !== 1){
     wm.innerHTML--;
+    lastValue = wm.innerHTML;
 }
 });
 
@@ -34,7 +68,7 @@ start.addEventListener('click', function(){
 });
 
 reset.addEventListener('click',function (){
-    wm.innerText = 25;
+    wm.innerText = lastValue;
     ws.innerText = '00';
 
     bm.innerText = 5;
@@ -43,6 +77,7 @@ reset.addEventListener('click',function (){
     document.getElementById('counter').innerText = 0;
     stopInterval();
     startTimer = undefined;
+    option = false;
 });
 
 stop.addEventListener('click',function(){
@@ -63,7 +98,13 @@ function timer(){
     
     // Contagem regressiva do parar
     if(wm.innerText == 0 && ws.innerText == 0){
+        if(option == false){
+        var notification = new Notification('Notification title', {
+            body: "Hey there! You've been notified!",
+        });
         document.getElementById('sound').play();
+        option = true;
+        }
 
         if(bs.innerText != 0){
             bs.innerText--;
@@ -74,13 +115,18 @@ function timer(){
     }
 
     if(wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0){
-        wm.innerText = 25;
+        document.getElementById('sound').play();
+        wm.innerText = lastValue;
         ws.innerText = '00';
 
-        bm.innerText = 5;
-        bs.innerText = '00';
+        bm.innerText = 0;
+        bs.innerText = '05';
+        option = false;
 
         document.getElementById('counter').innerText++;
+        if(counter % 4 == 0){
+            alert("Recomenda-se um intervalo de 10 minutos")
+        }
     }
 }
 
