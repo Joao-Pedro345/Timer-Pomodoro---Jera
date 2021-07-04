db82b782d0336a4ef209d4e4c83e32604a86993f
João Pedro Tavares <joao.pedro.ifms@gmail.com>
2021-07-04 14:54:07 -0400

    adicionada as funcionalidades de cada botão

--- main.js
+++ main.js
@@ -10,6 +10,33 @@ var bs = document.getElementById('b_seconds');
 
 var startTimer;
 
+start.addEventListener('click', function(){
+    if(startTimer === undefined){
+        startTimer = setInterval(timer,1000);
+    }else {
+        alert("ALERTA! O Timer já está em funcionamento");
+    }
+});
+
+reset.addEventListener('click',function(){
+    wm.innerText = 25;
+    ws.innerText = '00';
+
+    bm.innerText = 5;
+    bs.innerText = '00';
+
+    document.getElementById('counter').innerText = 0;
+    stopInterval();
+    startTimer = undefined;
+});
+
+stop.addEventListener('click',function(){
+    stopInterval();
+    startTimer = undefined;
+});
+
+
+
 function timer(){
     // Contagem regressiva do work
     if(ws.innerText != 0){
@@ -29,12 +56,16 @@ function timer(){
     }
 
     if(wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0){
-        wm.innerText == 25;
-        ws.innerText == '00';
+        wm.innerText = 25;
+        ws.innerText = '00';
 
-        bm.innerText == 5;
-        bs.innerText == '00';
+        bm.innerText = 5;
+        bs.innerText = '00';
 
         document.getElementById('counter').innerText++;
     }
+}
+
+function stopInterval(){
+    clearInterval(startTimer);
 }
